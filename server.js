89a25a946c0abc78b0703d7a83b41f5cd13a5dfd
Ramy Medhat <ramymedhat97@gmail.com>
2024-09-28 17:52:30 +0300

    "Added MongoDB connection and updated server response"

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
+const connectDB = require("./config/db");
+
+// Connect to MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
